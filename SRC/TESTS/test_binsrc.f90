dfe9305a86ab577ea261428a6e61afc4b34ced00
Daniel Forstenlechner <dforsti@gmail.com>
2022-01-23 19:14:23 +0100

    Rename the array in test_binsrc.f90

--- SRC/TESTS/test_binsrc.f90
+++ SRC/TESTS/test_binsrc.f90
@@ -7,12 +7,12 @@ contains
    @test
    subroutine test_binsrc_1()
       
-      double precision, dimension(10) :: A
+      double precision, dimension(10) :: p
       double precision :: xi = 5.
       integer :: i
 
-      A = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5]
-      call binsrc(A, 1, 10, xi, i)
+      p = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5]
+      call binsrc(p, 1, 10, xi, i)
       
       @assertEqual(5, i, message = "test binsrc")
 
